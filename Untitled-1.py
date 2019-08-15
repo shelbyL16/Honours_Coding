7a327b0aa2fbb08cdf10e45400295800b195677a
shelby <s.labuschagne96@gmail.com>
2019-08-15 21:00:16 +0200

    hit algorithm work with small sequence, not the long ones

--- Untitled-1.py
+++ Untitled-1.py
@@ -15,11 +15,11 @@ def load_data():
             # new_sequence = some_function(sequence)
             # write_fasta(out_file)
             sequence_map [name] = sequence#map the sequences to their names
-            length = len(sequence)#stored length as a variable
+            length = len(sequence)#stored length as a variable so that we can take away according to the lengths at some point
             if sequence_size_map.get(length):#fetch it if it is there, incase it aint there guys
                 sequence_size_map[length].append(name)#adding the name to the list of sequences with the same length
             else: 
-                sequence_size_map[length] = [name]#this is the first one in this case
+                sequence_size_map[length] = [name]#this is the first one in this case so add it to the dictionary
             sequence_map [name] = sequence#map the sequences to their names
             #print(name, i)
             #print(len(sequence),sequence, "\n")
@@ -27,3 +27,35 @@ def load_data():
         
         #for key in sequence_size_map:
         # print(key)
+        return sequence_map
+FRAME_SIZE = 3
+
+def get_sequence_similarity(sequence1, sequence2):
+    #saving subsequences found in a dictionary so that if we encounter it again at a later stage we can skip it
+    #increase efficiency of the program as we would have already done the work
+    #this is for the possible repitition of the sequences
+    score = 0
+    processed_sub_sequence = dict()
+    for i in range(len(sequence1) - FRAME_SIZE):#default is zero then it will go to the end of the sequenceS
+        nt_sequence = sequence1[i:i+FRAME_SIZE]#substring from i to the frame size
+        j = 0
+        if processed_sub_sequence.get(nt_sequence)  == True:
+            continue
+        processed_sub_sequence[nt_sequence] = True#if it is not found then the score is zero
+        while j != -1: #will terminate when it is not found
+            j = sequence2.find(nt_sequence, j) #find the sequence in the second sequence
+            if j != -1 :
+                score += 1
+                j += 1
+    return score
+
+sequence_map = load_data()
+keys = list(sequence_map.keys())
+# seq1 = "AGGTAGGATTCATT"
+# seq2 = "AGGTAGGATCCATT"
+seq1 = keys[0]
+seq2 = keys[1]
+print(sequence_map[seq1])
+print(sequence_map[seq2])
+
+print(get_sequence_similarity(seq1, seq2))
